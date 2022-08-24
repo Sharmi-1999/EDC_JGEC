df95df1c52283b5cb48e176aa86d70afa3e2ec86
Soham Chowdhury <77236783+code-soham@users.noreply.github.com>
2022-08-24 10:29:41 +0530

    bug

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+import NewsSlider from "../components/NewsSlider";
+import useSWR from "swr";
 export default function Home() {
-  return <div>EDC - Next.js</div>;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  let { data, error } = useSWR("api/static/news", fetcher);
+
+  console.log({ data, error });
+
+  return (
+    <div>
+      <NewsSlider data={data} error={error} />
+    </div>
+  );
 }
