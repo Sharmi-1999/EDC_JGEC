e55cae7accec15a28aba55a2c5f2df6ea88c0f2a
firtysh <sm2450@it.jgec.ac.in>
2022-08-30 13:17:22 +0530

    Dummy Signup

---
+++ pages/signup.jsx
@@ -0,0 +1,39 @@
+/********************************* 
+ *                               *
+ *                               *
+ *      Dummy Sign Up            *
+ *                               *
+ *                               *
+ *                               *
+ * *******************************/
+
+
+
+import { signIn} from "next-auth/react";
+
+const handleSignUp = () => {
+    let email = "a@b.c";
+    let password = "123456";
+    fetch('/api/user/signup', {
+        method: 'POST',
+        headers: {
+            "Accept": "application/json"
+        },
+        body: new URLSearchParams(`name=Suman Mandal&email=${email}&password=123456`)
+    })
+        .then(res => {
+            console.log(res.status);
+            if(res.status === 201){
+                signIn('credentials',{callbackUrl:"/",email:email,password:password})
+            }
+            return res.json()
+        }).then(res=>console.log(res))
+
+}
+
+export default function SignUp() {
+
+    return (
+        <button onClick={handleSignUp}>Sign Up</button>
+    )
+}
