776afa9adb12c2380408968947595cc07c56d718
Soham Chowdhury <77236783+code-soham@users.noreply.github.com>
2022-09-01 22:49:13 +0530

    blender

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
-import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
-import Appbar from '../components/Nav'
-function MyApp({
-  Component,
-  pageProps: {
-    session, ...pageProps
-  }
-}) {
-
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import Appbar from "../components/Nav";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
   return (
-    <SessionProvider session={session} refetchInterval={5*60}>
-      <Appbar/>
+    <SessionProvider session={session} refetchInterval={5 * 60}>
+      <Appbar />
       <Component {...pageProps} />
     </SessionProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,9 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import "animate.css";
+
 import Loader from "../components/Loader";
+import styled from "styled-components";
 export default function Home() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -13,36 +15,72 @@ export default function Home() {
   return loading ? (
     <Loader />
   ) : (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <div className="animate__animated animate__lightSpeedInLeft">
-        <Typography
-          sx={{
-            fontSize: "6vw",
-          }}
-          variant={"h2"}
-          fontFamily={"Cloister black"}
-        >
-          Entreprenuership & Development Cell
-        </Typography>
-      </div>
-      <div className="animate__animated animate__lightSpeedInRight">
-        <Typography
-          sx={{
-            fontSize: "4vw",
-          }}
-          variant={"h2"}
-          fontFamily={"Cloister black"}
-        >
-          Jalpaiguri Government Engineering College
-        </Typography>
-      </div>
-    </Box>
+    <>
+      <Box
+        sx={{
+          minHeight: "90vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          backgroundImage: `url("/assets/backgrounds/nasa_earth.jpg")`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        <div className="animate__animated animate__lightSpeedInLeft">
+          <Typography
+            sx={{
+              fontSize: "6vw",
+              textShadow: "6px 2px maroon",
+            }}
+            variant={"h2"}
+            fontFamily={"Cloister black"}
+          >
+            Entreprenuership & Development Cell
+          </Typography>
+        </div>
+        <div className="animate__animated animate__lightSpeedInRight">
+          <Typography
+            sx={{
+              fontSize: "4vw",
+              textShadow: "2px 2px maroon",
+            }}
+            variant={"h2"}
+            fontFamily={"Cloister black"}
+          >
+            Jalpaiguri Government Engineering College
+          </Typography>
+        </div>
+      </Box>
+      <Box
+        style={{
+          height: "100vh",
+          width: "100vw",
+          backgroundColor: "white",
+          padding: "50px",
+        }}
+      >
+        <Typography variant="h1" sx={{
+        }} fontFamily={"Comic Sans MS"} color="maroon">Our Vision</Typography>
+        <VisionCard>
+          <Img src="/assets/placeholders/compass.jpg" />
+        </VisionCard>
+      </Box>
+    </>
   );
 }
+const VisionCard = styled.div`
+  width: 80%;
+  margin: auto;
+  minwidth: 360px;
+  height: 30vh;
+  background-color: #f5f5f5;
+`;
+const Img = styled.img`
+  height: 100%;
+  width: 20%;
+  object-fit: cover;
+`;
