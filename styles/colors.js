3738c81010c47e13ff896012e9b6312f80cfdf8e
Soham Chowdhury <77236783+code-soham@users.noreply.github.com>
2022-09-03 23:43:20 +0530

    Cleansheet

---
+++ styles/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: "#9f0037",
+  secondary: "#282533",
+  white: "#fff",
+  black: "#000",
+  grey: "#ccc",
+  lightGrey: "#f5f5f5",
+  darkGrey: "#333",
+  danger: "#ff5252",
+  success: "#4ecdc4",
+  warning: "#ffe66d",
+  info: "#6ab04c",
+};
